255b765935628c053edc93cb7f4c3fd76e0a2978
guybrush <guybrush@jordan>
2020-04-15 23:50:41 +0200

    Adds coverage support
    Adds coverage support through lcov when run make check.

---
+++ test/cover.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+../test/optparse_test
+result="$?"
+if [ "$result" -ne 0 ]; then
+    exit $result
+else
+    cd ./src
+    lcov --directory . --capture --output-file liboptparse.info
+    genhtml liboptparse.info
+fi
